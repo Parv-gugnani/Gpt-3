aa228c6df85604ae60b57bc63c975f01a0aac9aa
Parv-gugnani <9765.parv@gmail.com>
2023-06-29 22:34:50 +0530

    error-up

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ import {
   Header,
 } from "./containers";
 
-import { Brand, CTA, Navbar } from "./components";
+import { CTA, Brand, Navbar } from "./components";
 
 const App = () => {
   return (
     <div className="App">
-      <div className="gradient_bg">
+      <div className="gradient__bg">
         <Navbar />
         <Header />
       </div>
