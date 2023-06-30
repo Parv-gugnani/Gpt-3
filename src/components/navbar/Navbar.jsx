4057ec56de4bbbc4b619820b7bb3c166542362dc
Parv-gugnani <9765.parv@gmail.com>
2023-06-30 15:03:08 +0530

    figma

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from "react";
+import { RiMenu3Line, RiClossLin } from "react-icon/ri";
+import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
+//BEM
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return;
+  <div className="gpt3__navbar">
+    <div className="gpt3__navbar-links">
+      <div className="gpt3__navbar-links_logo">
+        <img src={logo} alt="logo" />
+      </div>
+    </div>
+  </div>;
 };
 
 export default Navbar;
