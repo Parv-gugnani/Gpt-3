2e69e76e7c0ddf0e77a4c0ea859c77a0c2b5adcd
Parv-gugnani <9765.parv@gmail.com>
2023-06-30 15:59:29 +0530

    errors!

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RiMenu3Line, RiClossLin } from "react-icon/ri";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
 import "./navbar.css";
 
@@ -10,7 +10,7 @@ const Navbar = () => {
   <div className="gpt3__navbar">
     <div className="gpt3__navbar-links">
       <div className="gpt3__navbar-links_logo">
-        <img src={logo} alt="logo" />
+        <img src={logo} />
       </div>
     </div>
   </div>;
