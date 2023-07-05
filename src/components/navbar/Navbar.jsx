e4dadbc71612f817cfa703d118a2b45f744606fe
Parv-gugnani <9765.parv@gmail.com>
2023-07-05 14:36:26 +0530

    location update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,24 +1,3 @@
-// import React from "react";
-// import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
-// import logo from "../../assets/logo.svg";
-// import "./navbar.css";
-
-// //BEM
-
-// const Navbar = () => {
-//   return (
-//     <div className="gpt3__navbar">
-//       <div className="gpt3__navbar-links">
-//         <div className="gpt3__navbar-links_logo">
-//           <img src={logo} />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
 import React from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
@@ -32,6 +11,23 @@ const Navbar = () => {
       <div className="gpt3__navbar-links">
         <div className="gpt3__navbar-links_logo">
           <img src={logo} />
+        </div>
+        <div className="gpt3__navbar-links_container">
+          <p>
+            <a href="#home">Home</a>
+          </p>
+          <p>
+            <a href="#wgpt3">What is Gpt ?</a>
+          </p>
+          <p>
+            <a href="#possibility">Open AI</a>
+          </p>
+          <p>
+            <a href="#features">Case Studies</a>
+          </p>
+          <p>
+            <a href="#blog">Library</a>
+          </p>
         </div>
       </div>
     </div>
